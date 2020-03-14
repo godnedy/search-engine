284f75ac6216c267f9621e32318e18e661958106
Edyta Godniak (ego01) <edyta.godniak@hrs.com>
2020-03-14 16:38:40 +0100

    initial commit

---
+++ src/main/java/com/findwise/searchengine/SearchEngine.java
@@ -0,0 +1,13 @@
+package com.findwise.searchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchEngine {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchEngine.class, args);
+    }
+
+}
